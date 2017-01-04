83cd295c815cff92b64063837e0c798a06ad82f8
lucypoly <milla100@mail.ru>
2017-01-04 20:42:07 +0200

    handlebars added menu added

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,37 @@ const browserSync = require('browser-sync').create();
 const browserify = require('gulp-browserify');
 const imagemin = require('gulp-imagemin');
 const imageResize = require('gulp-image-resize');
+const handlebars = require('gulp-compile-handlebars');
+const fs = require('fs');
+const rename = require("gulp-rename");
+
+
+// HANDLEBARS
+
+// make data and partials available in project
+// compile handlebars templates - place in dist as .html files
+
+gulp.task('handlebars', function () {
+    var data = JSON.parse(fs.readFileSync('app/data/data.json'));
+    // var templateData = {}, // data to pass into templates - using ./data/data.json above
+    options = {
+        ignorePartials: true, // ignores any unknown partials. Useful if you only want to handle part of the file
+        // partials : {}, // Javascript object that will fill in partials using strings
+        batch: ['app/partials'] // Javascript array of filepaths to use as partials
+        // helpers : {
+        //     capitals : function(str){
+        //         return str.toUpperCase();
+        //     }
+        // } // javascript functions to stand in for helpers used in the handlebars files
+    };
+    return gulp.src('app/templates/**/*.hbs')
+        .pipe(handlebars(data, options)) // using ./data/data.json file
+        .pipe(rename(function (path) {
+            path.extname = '.html';
+        }))
+        .pipe(gulp.dest('app/dist'))
+        .pipe(livereload())
+});
 
 gulp.task('img', () =>
     gulp.src('app/img/**/*')
@@ -30,7 +61,7 @@ gulp.task('sass', function () {
 
 
 gulp.task('js', function () {
-    return gulp.src('app/js/**/*.js')
+    return gulp.src('app/js/*.js')
         .pipe(browserify())
         .pipe(gulp.dest('app/dist/js'));
 });
@@ -41,17 +72,20 @@ gulp.task('application-watch', ['js'], function (done) {
 });
 
 
-gulp.task('serve', ['sass', 'js'], function () {
+gulp.task('serve', ['sass', 'js', ], function () {
     browserSync.init({
         server: 'app'
     });
 
     gulp.watch(['app/sass/main.scss'], ['sass']);
-    gulp.watch('app/src/**/*.js', ['application-watch']);
+    gulp.watch('app/js/*.js', ['application-watch']);
     gulp.watch(['app/*.html']).on('change', browserSync.reload);
+    gulp.watch(['app/templates/**'], ['handlebars']);
+    gulp.watch(['app/partials/*.hbs'], ['handlebars']);
+    gulp.watch(['app/data/data.json'], ['handlebars']);
 
 });
 
-gulp.task('default', ['serve', 'img'], function () {
+gulp.task('default', ['serve', 'img', 'handlebars'], function () {
     console.log('gulp is watching...')
 });
