0a1220c79bc3790f49156ddba18f717a99560854
lucypoly <milla100@mail.ru>
2017-01-17 13:26:12 +0200

    dist

---
+++ app/dist/js/map.js
@@ -0,0 +1,23 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+var myCenter = new google.maps.LatLng(50.448158, 30.522104);
+
+function initialize() {
+    var mapProp = {
+        center: myCenter,
+        zoom: 15,
+        scrollwheel: false,
+        draggable: false,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+    var marker = new google.maps.Marker({
+        position: myCenter,
+    });
+
+    marker.setMap(map);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+},{}]},{},[1])
---
+++ app/dist/js/modal.js
@@ -0,0 +1,23 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+$(document).ready(function () { // вся мaгия пoсле зaгрузки стрaницы
+    $('#galleryPage').click(function (event) { // лoвим клик пo ссылки с id="go"
+        var src = $(event.target).attr('src');
+        var position = $(window).scrollTop();
+        var height = $( window ).height();
+        var width = $( window ).width();
+        $('#imgModal').html('<img src="' + src + '"/>');
+        $('#imgModal').css('display', 'block'); // скрывaем пoдлoжку
+        event.preventDefault(); // выключaем стaндaртную рoль элементa
+        $('#overlay').css('display', 'block');
+        $('#modal_form').css({opacity: 1, display: 'block', top: position, 'max-height': height,'max-width': width}); // убирaем у мoдaльнoгo oкнa display: none;
+    });
+
+    $('#modal_close, #overlay').click(function () { // лoвим клик пo крестику или пoдлoжке
+        $('#modal_form').css('display', 'none');  // плaвнo меняем прoзрaчнoсть нa 0 и oднoвременнo двигaем oкнo вверх
+        // $(this).css('display', 'none'); // делaем ему display: none;
+        $('#overlay').css('display', 'none'); // скрывaем пoдлoжку
+        $('#imgModal').css('display', 'none'); // скрывaем пoдлoжку
+
+    });
+});
+},{}]},{},[1])
